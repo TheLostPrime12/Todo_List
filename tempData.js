9ff75be8b7262495084eaadab2338f9e262aa1cf
Tanish Garg <83292277+TheLostPrime12@users.noreply.github.com>
2022-10-26 17:10:47 +0530

    Setting and Displaying Lists

---
+++ tempData.js
@@ -0,0 +1,70 @@
+// Temporary Data untill we connect with the firebase
+
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: false
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Buy Eggs",
+                completed: true
+            },
+            {
+                title: "Buy Bread",
+                completed: true
+            },
+            {
+                title: "Buy Butter",
+                completed: false
+            },
+            {
+                title: "Buy Apples",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Get Baloons",
+                completed: false
+            },
+            {
+                title: "Send Invitations",
+                completed: true
+            },
+            {
+                title: "Make Diner Reservations",
+                completed: false
+            }
+        ]
+    },
+]
